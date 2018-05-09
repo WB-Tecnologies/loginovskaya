abb91b319e676d329057fca9df3d75377a3d75ae
crazyzubr <hjcnbckfd@gmail.com>
2018-05-09 22:06:20 +0300

    :rocket: work checked social icons in first step form

--- inquirers/tests/tests_wizard_first_step.py
+++ inquirers/tests/tests_wizard_first_step.py
@@ -1,3 +1,4 @@
+from django.templatetags.static import static
 from django.urls import reverse, reverse_lazy
 
 from helpers.base_test import BaseTestCase
@@ -148,6 +149,42 @@ class WizardFirstStepTestCase(BaseTestCase):
         """ Check that missing button to previous step """
         response = self.client.get(self.URL)
         self.assertFalse(response.get_html().find(class_='c-step-previous'))
+
+    def test_correct_socials_icons(self):
+        """ Check that display correct socials icons """
+        socials_choices = FirstForm.SOCIALS_CHOICES
+        response = self.client.get(self.URL)
+        socials_icons = response.get_html().find_all('img', class_='btn-socials')
+        self.assertEqual(len(socials_icons), len(socials_choices))
+        for i, choice in enumerate(socials_choices):
+            value, label = choice
+            with self.subTest(label=label):
+                self.assertEqual(socials_icons[i]['src'], static('img/{}.svg'.format(value)))
+                self.assertEqual(socials_icons[i]['alt'], label)
+
+    def test_attribute_data_value_in_socials_links_as_socials_checkbox_value(self):
+        """ Check that attribute data-value in socials links as socials checkbox value """
+        socials_choices = FirstForm.SOCIALS_CHOICES
+        response = self.client.get(self.URL)
+        socials_links = response.get_html().find_all('a', class_='c-socials-link')
+        self.assertEqual(len(socials_links), len(socials_choices))
+        for i, choice in enumerate(socials_choices):
+            value, _ = choice
+            with self.subTest(value=value):
+                self.assertEqual(socials_links[i]['data-value'], value)
+
+    def test_socials_icons_marked_as_active_if_checked(self):
+        """ Check that socials icons has active css class if checked """
+        field_name = self._get_field_name('socials')
+        checked_values = ['vkontakte', 'instagram']
+        response = self.client.post(self.URL, data=self._get_only_data(**{field_name: checked_values}))
+        self.assertEqual(response.status_code, 200)
+        icons_active = response.get_html().find_all('img', class_='btn-active')
+        self.assertEqual(len(icons_active), len(checked_values))
+        for i, value in enumerate(checked_values):
+            label = dict(FirstForm.SOCIALS_CHOICES)[value]
+            with self.subTest(value=value):
+                self.assertEqual(icons_active[i]['alt'], label)
 
     def _get_field_name(self, name):
         return '{}-{}'.format(self.STEP, name)

f91de3632b8d8e298bb6e6974e6e53f944498036
crazyzubr <hjcnbckfd@gmail.com>
2018-05-09 12:55:58 +0300

    :repeat: upgrade to Django 2.0.5

--- loginovskaya/urls.py
+++ loginovskaya/urls.py
@@ -1,35 +1,19 @@
-"""loginovskaya URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import url, include
+from django.conf.urls import include
 from django.contrib import admin
 from django.conf import settings
+from django.urls import path
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    url(r'^viewing-inside/', admin.site.urls),
+    path('viewing-inside/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html')),
+    path('1', TemplateView.as_view(template_name='steps/1.html')),
+    path('2', TemplateView.as_view(template_name='steps/2.html')),
+    path('3', TemplateView.as_view(template_name='steps/3.html')),
+    path('4', TemplateView.as_view(template_name='steps/4.html')),
+    path('5', TemplateView.as_view(template_name='steps/5.html')),
 ]
 
 if settings.DEBUG:
     import debug_toolbar
-    urlpatterns = [
-        url(r'^$', TemplateView.as_view(template_name='index.html')),
-        url(r'^1$', TemplateView.as_view(template_name='steps/1.html')),
-        url(r'^2$', TemplateView.as_view(template_name='steps/2.html')),
-        url(r'^3$', TemplateView.as_view(template_name='steps/3.html')),
-        url(r'^4$', TemplateView.as_view(template_name='steps/4.html')),
-        url(r'^5$', TemplateView.as_view(template_name='steps/5.html')),
-        url(r'^__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
+    urlpatterns = [path('__debug__/', include(debug_toolbar.urls))] + urlpatterns
